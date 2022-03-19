2a5b34072e6f4d7a437942d80c966cddf80a374f
chris.butler <christopher.butler@dataiku.com>
2022-03-19 10:40:56 -0600

    Created python recipe: DKU_CHURN.recipe_from_notebook_chris_butlers_Python_notebook_on_unlabeled_customers_prepared

---
+++ recipes/recipe_from_notebook_chris_butlers_Python_notebook_on_unlabeled_customers_prepared.py
@@ -0,0 +1,25 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE_MAGIC_CELL
+# Automatically replaced inline charts by "no-op" charts
+# %pylab inline
+import matplotlib
+matplotlib.use("Agg")
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+import dataiku
+from dataiku import pandasutils as pdu
+import pandas as pd
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Read the dataset as a Pandas dataframe in memory
+# Note: here, we only read the first 100K rows. Other sampling options are available
+dataset_unlabeled_customers_prepared = dataiku.Dataset("unlabeled_customers_prepared")
+df = dataset_unlabeled_customers_prepared.get_dataframe(limit=100000)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Get some simple descriptive statistics
+pdu.audit(df)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Recipe outputs
+nb_to_recipe_dataset = dataiku.Dataset("nb_to_recipe_dataset")
+nb_to_recipe_dataset.write_with_schema(pandas_dataframe)
